6af1b44c46031989697f2765068b2c4bf30ac36f
sarim2000 <sarimbleedblue@gmail.com>
2024-01-07 16:15:30 +0530

    Add .gitignore file and implement increasingArray.cpp

---
+++ cses/increasingArray.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    long long int cnt = 0;
+    int i = 1;
+    for (int i = 1; i < n; i++){
+        if (a[i] < a[i - 1]){
+            cnt += a[i - 1] - a[i];
+            a[i] = a[i - 1];
+        }
+    }
+    cout << cnt;
+    return 0;
+}
